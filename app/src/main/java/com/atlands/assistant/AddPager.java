b938e9eb3fcc0517df5f61afeb38d8c6e75dc88b
Atlands <xnyonianchen@outlook.com>
2019-06-25 15:01:46 +0800

    新增addpager页

---
+++ app/src/main/java/com/atlands/assistant/AddPager.java
@@ -0,0 +1,13 @@
+package com.atlands.assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pager);
+    }
+}
